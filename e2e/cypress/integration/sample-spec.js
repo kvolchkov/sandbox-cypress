dbea431e2bf426ce92b43d960c2327d1bcb78412
Kostiantyn Volchkov <Kostiantyn.Volchkov@geutebrueck.com>
2021-01-27 16:34:19 +0100

    test: Added failing test

--- e2e/cypress/integration/sample-spec.js
+++ e2e/cypress/integration/sample-spec.js
@@ -8,4 +8,8 @@ describe('Angular app test', () => {
   it('should get correct title', () => {    
     cy.title().should('eq', "Angular 9 Example App");
   })
+
+  it('should fail', () =>{
+    cy.get('xxx-home-page h3').contains('Written in Angular 11');
+  })
 })
