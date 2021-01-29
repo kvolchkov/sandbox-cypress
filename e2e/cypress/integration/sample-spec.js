ab13a365ca09c4bc50dbc546002d55479cf0ad06
Kostiantyn Volchkov <Kostiantyn.Volchkov@geutebrueck.com>
2021-01-29 15:25:46 +0100

    test: Added test

--- e2e/cypress/integration/sample-spec.js
+++ e2e/cypress/integration/sample-spec.js
@@ -9,7 +9,13 @@ describe('Angular app test', () => {
     cy.title().should('eq', "Angular 9 Example App");
   })
 
-  it('should fail', () =>{
-    cy.get('xxx-home-page h3').contains('Written in Angular 11');
+  it('should not contain text', () =>{
+    cy.get('xxx-home-page h3').should('not.contain.text','Written in Angular 11');
+  })
+
+  it('should return search results', () => {
+    cy.get('app-root mat-toolbar form input').type('Cypress');
+    cy.get('app-root mat-toolbar form button').click();
+    cy.get('xxx-questions-page a').should('contain.text', 'Cypress');
   })
 })
